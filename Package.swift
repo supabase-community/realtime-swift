22d66acd5f7adc68d8e1f243eab042d3b6cfc0f5
Guilherme Souza <grsouza@pm.me>
2022-09-05 18:00:56 -0300

    Bump swift-tools-version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
   name: "Realtime",
   platforms: [
-    .macOS(.v10_15),
     .iOS(.v13),
+    .macCatalyst(.v13),
+    .macOS(.v10_15),
     .watchOS(.v6),
     .tvOS(.v13),
   ],
@@ -17,7 +18,6 @@ let package = Package(
       targets: ["Realtime"]
     )
   ],
-  dependencies: [],
   targets: [
     .target(
       name: "Realtime",
