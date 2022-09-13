0b985c687fe963f6bd818ff77a35c27247b98bb4
Guilherme Souza <grsouza@pm.me>
2022-09-13 05:32:31 -0300

    Remove Starscream Dependency and bump minimum support platform (#11)
    * Remove Starscream
    
    * Bump swift-tools-version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,23 +1,27 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "Realtime",
+  platforms: [
+    .iOS(.v13),
+    .macCatalyst(.v13),
+    .macOS(.v10_15),
+    .watchOS(.v6),
+    .tvOS(.v13),
+  ],
   products: [
     .library(
       name: "Realtime",
       targets: ["Realtime"]
     )
   ],
-  dependencies: [
-    .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.0")
-  ],
   targets: [
     .target(
       name: "Realtime",
-      dependencies: ["Starscream"]
+      dependencies: []
     ),
     .testTarget(
       name: "RealtimeTests",
