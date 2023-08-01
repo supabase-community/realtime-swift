478c72e58908c05ba61d00f6e77b22c394fb1950
Brian R <brian@antimatter.systems>
2023-07-31 23:01:48 -0400

    remove endpoint parameter name underscore prefix

--- Sources/Realtime/RealtimeClient.swift
+++ Sources/Realtime/RealtimeClient.swift
@@ -167,7 +167,7 @@ public class RealtimeClient: TransportDelegate {
   // ----------------------------------------------------------------------
   @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
   public convenience init(
-    _ endPoint: String,
+    endPoint: String,
     params: Payload? = nil,
     vsn: String = Defaults.vsn
   ) {
@@ -181,7 +181,7 @@ public class RealtimeClient: TransportDelegate {
 
   @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
   public convenience init(
-    _ endPoint: String,
+    endPoint: String,
     paramsClosure: PayloadClosure?,
     vsn: String = Defaults.vsn
   ) {
